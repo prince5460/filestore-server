cad961ab63c7821dd05764ca4942fbfffac20259
prince5460 <prince5460@126.com>
2019-10-14 16:01:56 +0800

    文件元信息的数据库写入及查询

--- meta/filemeta.go
+++ meta/filemeta.go
@@ -1,5 +1,9 @@
 package meta
 
+import (
+	mydb "filestore-server/db"
+)
+
 //FileMeta:文件元信息结构
 type FileMeta struct {
 	FileSha1 string
@@ -20,12 +24,33 @@ func UpdateFileMeta(fmeta FileMeta) {
 	fileMetas[fmeta.FileSha1] = fmeta
 }
 
+// UpdateFileMetaDB : 新增/更新文件元信息到mysql中
+func UpdateFileMetaDB(fmeta FileMeta) bool {
+	return mydb.OnFileUploadFinished(
+		fmeta.FileSha1, fmeta.FileName, fmeta.FileSize, fmeta.Location)
+}
+
 //GetFileMeta:通过sha1值获取文件的元信息对象
 func GetFileMeta(fileSha1 string) FileMeta {
 	return fileMetas[fileSha1]
 }
 
+//GetFileMetaDB:从mysql获取文件元信息
+func GetFileMetaDB(fileSha1 string) (FileMeta, error) {
+	tfile, err := mydb.GetFileMeta(fileSha1)
+	if err != nil {
+		return FileMeta{}, err
+	}
+	fmeta := FileMeta{
+		FileSha1: tfile.FileHash,
+		FileName: tfile.FileName.String,
+		FileSize: tfile.FileSize,
+		Location: tfile.FileAddr.String,
+	}
+	return fmeta, nil
+}
+
 //RemoveFileMeta:删除元信息
-func RemoveFileMeta(fileSha1 string)  {
-	delete(fileMetas,fileSha1)
+func RemoveFileMeta(fileSha1 string) {
+	delete(fileMetas, fileSha1)
 }
